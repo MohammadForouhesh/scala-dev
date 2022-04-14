c36d8b079aa5367ed757c6f009d1151fbd688877
keif-forouhesh <mohammadh.forouhesh@gmail.com>
2022-04-15 03:45:42 +0430

    Update basics.scala

--- src/main/scala/basics.scala
+++ src/main/scala/basics.scala
@@ -1,7 +1,7 @@
 
 def isInteresting(numbers: List[Int], t: Int, k: Int): Unit = {
-    for {ind <- 1 to k-1} do {
-        val difference = for index <- 1 to (numbers.length - ind) yield numbers(index) - numbers(index + ind)
-        println(difference)
+    for {ind <- 1 to k} do {
+        val difference = for index <- 0 until (numbers.length - ind - 1) yield math.abs(numbers(index) - numbers(index + ind))
+        difference.exists(item => item < t)
     }
 }
