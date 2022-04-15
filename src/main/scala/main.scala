25be707d3afc540df66ad284ade161249ece47f0
keif-forouhesh <mohammadh.forouhesh@gmail.com>
2022-04-15 22:54:09 +0430

    Update main.scala

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,4 +3,7 @@ def main(): Unit = {
   assert(isInteresting(List(1, 2, 3, 4), 3, 3))
   assert(isInteresting(List(1, 0, 1, 1), 2, 1))
   assert(! isInteresting(List(1, 5, 9, 1, 5, 9), 2, 2))
+
+  assert(pattern_matching("abba", "cat dog dog cat"))
+  assert(! pattern_matching("abba", "cat dog horse cat"))
 }
