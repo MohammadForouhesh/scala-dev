e715e32a4777698e821bcfb80a91aa1aca3a572b
keif-forouhesh <mohammadh.forouhesh@gmail.com>
2022-04-15 22:51:57 +0430

    Update basics.scala

--- src/main/scala/basics.scala
+++ src/main/scala/basics.scala
@@ -10,3 +10,17 @@ def isInteresting(numbers: List[Int], t: Int, k: Int): Boolean = {
 }
 
 
+def pattern_matching(rule: String, stream: String): Boolean = {
+    val split_rule: Array[String]     = rule.split("")
+    val split_stream: Array[String]   = stream.split(" ")
+    var pattern: Map[String, String]  = Map(split_rule(0) -> split_stream(0))
+    for ind <- split_rule.indices do {
+        if pattern.contains(split_rule(ind)) then
+            if pattern(split_rule(ind)) != split_stream(ind) then
+                return false
+
+        else pattern += (split_rule(ind) -> split_stream(ind))
+    }
+    true
+}
+
