a26288b48e36e1b90088695d9a1762f114d98e23
mforouhesh <17898264+MohammadForouhesh@users.noreply.github.com>
2022-09-15 15:25:50 +0430

    Update basics.scala

--- src/main/scala/basics.scala
+++ src/main/scala/basics.scala
@@ -1,4 +1,4 @@
-def isInteresting(numbers: List[Int], t: Int, k: Int): Boolean = {
+def is_interesting(numbers: List[Int], t: Int, k: Int): Boolean = {
     for {ind <- 1 to k} do {
         val difference = for index <- 0 until (numbers.length - ind - 1) yield math.abs(numbers(index) - numbers(index + ind))
         if difference.exists(item => item < t) then
