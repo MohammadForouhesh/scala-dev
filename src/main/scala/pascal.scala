02730227a0f78ccba8394af8eb5505545805a7d4
mforouhesh <17898264+MohammadForouhesh@users.noreply.github.com>
2022-09-26 03:41:15 +0330

    Create pascal.scala

---
+++ src/main/scala/pascal.scala
@@ -0,0 +1,12 @@
+
+def pascal(c: Int, r: Int): Int =
+    if r == 0 || c == 0 || c == r then 1 else pascal(c, r - 1) + pascal(c - 1, r - 1)
+
+
+@main
+def triangle: Unit =
+    println("Pascal's Triangle")
+    for row <- 0 to 10 do
+        for col <- 0 to row do
+            print(s"${pascal(col, row)} ")
+        println()
