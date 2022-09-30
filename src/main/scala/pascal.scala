ab45fed01ad94d81129c6041b339ce2cd23e6f25
mforouhesh <17898264+MohammadForouhesh@users.noreply.github.com>
2022-09-30 20:14:43 +0330

    Update pascal.scala

--- src/main/scala/pascal.scala
+++ src/main/scala/pascal.scala
@@ -7,6 +7,7 @@ def pascal(c: Int, r: Int): Int =
 def triangle: Unit =
     println("Pascal's Triangle")
     for row <- 0 to 10 do
+        print(" "*(10 - row))
         for col <- 0 to row do
             print(s"${pascal(col, row)} ")
         println()
